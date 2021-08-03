e8ce2870e81b663979adb425bb991f230ea6f821
Juliano_RIC <jrs96.jr@gmail.com>
2021-08-02 23:22:01 -0300

    Cascata FINAL e mudancas no item 1

--- Tarefa 2/Item 3/ControladorCascataMalhaInterna.m
+++ Tarefa 2/Item 3/ControladorCascataMalhaInterna.m
@@ -2,11 +2,6 @@ clear all
 close all
 clc
 
-
-%% Parametros da planta
-Ke = 1.05;    %ganho estatico da planta
-tau = 0.5;   %constante de tempo da equação linearizada perto do ponto de equilibrio
-
 %% Especificacoes MF
 t_5 = 0.15; %tempo de 5% [minutos]
 pico = 0.05; %sobressinal 5%
@@ -30,7 +25,7 @@ sd_imNegativo = -fa*wn -  1i*wn*sqrt(1-fa^2);  %parte real negativa
 
 
  fase1 = 180 - rad2deg(double(atan( abs(imag(sd))/(-1*real(sd))))); %polo Controlador na origem
- fase2 = 180 - rad2deg(double(atan( abs(imag(sd))/(- 1*real(sd)-6.9433)))); %polo em 6.9433
+ fase2 = 180 - rad2deg(double(atan( abs(imag(sd))/(-1*real(sd)-6.9433)))); %polo em 6.9433
  
 faseConhecida = -(fase1+fase2) ;
 
@@ -39,27 +34,41 @@ faseTotal = -180;
 syms faseDesconhecida
 faseDesconhecida = vpa(solve(faseTotal == faseConhecida + faseDesconhecida));
 %%
-zero =  -1*real(sd) +  (imag(sd))/tan(deg2rad(double( (faseDesconhecida))));
+zero = double( -1*real(sd) +  (imag(sd))/tan(deg2rad(double( (faseDesconhecida)))));
 %Prova real
-fasePROVAreal = rad2deg(double(atan( abs(imag(sd))/(zero))));
+fasePROVAreal =  rad2deg(double(atan( abs(imag(sd))/(zero + real(sd)))));
 
 %% Calculando K
 syms K 
 
 s = sd_imPositivo;
-Kc_pos =  double(vpa(solve( 1 + K*4.5067*(s+zero)/(s*(s+6.9433))  == 0)));
+Kc_pos =  double(vpa(solve( 1 + (K*4.5067*(s+zero))/(s*(s+6.9433))  == 0)));
 s = sd_imNegativo;
-Kc_neg =  double(vpa(solve( 1 + K*4.5067*(s+zero)/(s*(s+6.9433))  == 0)));
+Kc_neg =  double(vpa(solve( 1 + (K*4.5067*(s+zero))/(s*(s+6.9433))  == 0)));
 
 Kc = real(Kc_pos);
 
 %% Funcoes de transferencia contínuas
 s = tf([1 0],[1]); % variavel de Laplace
-Pint = 4.5067/(s+6.9433);
-C = 7.335*(s+25.4)/s;
-F = 25.4/(s+25.4); 
-Gmf = C*Pint*F/(1+C*Pint);
-GmfP = (0.8/(s+6.9433))/(1+C*Pint);
+
+Pint = 4.5067/(s+6.9433); %malha interna
+C = Kc*(s+zero)/s;  %controlador
+
+figure
+rlocus(C*Pint)
+
+Gm = (C*Pint)/(1+C*Pint); %para Y/R
+figure
+step(Gm)
+GmfP = (0.8/(s+6.9433))/(1+C*Pint); %para Y/Q
+
+%filtro: precisa? Malha interna é pra perturbacao
+%F = 25.4/(s+25.4);
+%Gmf = (C*Pint*F)/(1+C*Pint); %para Y/R com filtro
+
+%figure
+%step(Gmf)
+figure
 step(GmfP)
 
 
