d05cab6e2f3d48d5daecaf511236e50b1d44bb7f
Juliano_RIC <jrs96.jr@gmail.com>
2021-09-18 10:58:02 -0300

    Item 3 - Controlador Equivalente

--- Tarefa 3/Item 3/item3_PSF_comK.m
+++ Tarefa 3/Item 3/item3_PSF_comK.m
@@ -40,7 +40,7 @@ Hsf=  zpk(minreal((C_d*Gu_d)/(1+C_d*Gu_d),1e-2)*(z^-40));
 
 %funcao de transferencia de malha fechada de Y/Q
 Hqsf = zpk(minreal(((Gq_d)*(z^-40))*(1 - Hsf)));
-[num_Hq,den_Hq] = tfdata(Hq);
+[num_Hqsf,den_Hqsf] = tfdata(Hqsf);
 %step(Hq)
 
 %% Achando params filtro
@@ -48,16 +48,16 @@ syms alpha beta gamma K
 
 %calculando alpha
 z = 0.884;
-NumColchete = ((z^40)*((z-0.678)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
+NumColchete = ((z^40)*((z-0.68)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
 d = 4; %precisao 
 valor_alpha = vpa(solve((NumColchete == 0), alpha), d);
 % calculando beta 
 z = 0.5941;
-NumColchete2 = ((z^40)*((z-0.678)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
+NumColchete2 = ((z^40)*((z-0.68)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
 valor_beta = vpa(solve( (NumColchete2 == 0), beta), d);
 % calculando gamma
 z = 1;
-NumColchete3 = ((z^40)*((z-0.678)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
+NumColchete3 = ((z^40)*((z-0.68)^4)) + (0.32895*(z-1.543)*(alpha*(z^2)+(beta*z)+gamma));
 valor_gamma = vpa(solve((NumColchete3 == 0), gamma), d);
 z = 1;
 NumColchete4 =  ((alpha*(z^2) + beta*z + gamma)*(K)*(z^2 - 1.628*z + 0.7312)*(z^2 - 1.239*z + 0.4697))/(  ((z-0.68)^4)*(z-0.7476)^2);
@@ -79,7 +79,8 @@ K = double(vpa(sol.K))
 
 %% Testando filtro
 z = tf('z')
-Fr = zpk(((alpha*(z^2) + beta*z + gamma)*(K)*(z^2 - 1.628*z + 0.7312)*(z^2 - 1.239*z + 0.4697))/(  ((z-0.68)^4)*(z-0.7476)^2))
+%Fr = zpk(((alpha*(z^2) + beta*z + gamma)*(K)*(z^2 - 1.628*z + 0.7312)*(z^2 - 1.239*z + 0.4697))/(  ((z-0.68)^4)*(z-0.7476)^2))
+Fr = ((alpha*(z^2) + beta*z + gamma)*(K)*(z^2 - 1.628*z + 0.7312)*(z^2 - 1.239*z + 0.4697))/(  ((z-0.68)^4)*(z-0.7476)^2)
 dcgain(Fr)
 Hq = zpk(minreal(((Gq_d)*(z^-40))*(1 - Hsf*Fr)));
 [num_Hq,den_Hq] = tfdata(Hq);
@@ -90,35 +91,11 @@ hold on
 step(Hq)
 legend('Sem filtro', 'com filtro')
 
-%% Configuraçao janela
+%% Implementar Ceq 
+%Ceq = zpk(minreal((C_d*Fr)/(1+ C_d*Gu_d*(1-Fr*z^-40)), 1e-2))
 
-%Gerando graficos da questao 2
-% screenSize = get(0,'screensize'); % gets screen size
-%  sim('item1PS')
-% 
-%monWidth = screenSize(3);
-% monHeight = screenSize(4);
-% offHeight = 0; % assumed height of system task bar
-% monHeight = monHeight - offHeight; % usable screen height
-% % establishing a 2x3 grid on the screen
-% figHeight = monHeight/2;
-% figWidth = monWidth/3;
-
-
-% figure
-% set(gcf,'OuterPosition',[1 offHeight figWidth figHeight]);
-% set(gcf,'name','Resposta MF')
-% plot(sp.Time,sp.Data, '- r')
-% hold on
-% plot(pv.Time, pv.Data, '-b')
-% hold on
-% plot(pert.Time,pert.Data)
-% %axis([0 121 0 4])
-% legend('SP', 'PV', 'pert')
-% xlabel('Tempo [min]')
-% ylabel('Concentracao Produto B [mol/L]')
-% title('Resposta dinamica para a Cb')
-
+Ceq = minreal((C_d*Fr)/(1+ C_d*Gu_d*(1-Fr*z^-40)), 1e-2)
+%NovoF = F_d/Fr;
 
 
  
